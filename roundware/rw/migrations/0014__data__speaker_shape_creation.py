b56fd66d99338ca4ba5c8ff3b085845123ca6150
evan <evan@nonchalance.com>
2016-01-21 11:58:02 -0800

    `Speaker.attenuation_border` is a line, not a polygon. calculate derivative fields as part of data migration. Fixes #5

--- roundware/rw/migrations/0014__data__speaker_shape_creation.py
+++ roundware/rw/migrations/0014__data__speaker_shape_creation.py
@@ -4,6 +4,13 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+def save_speakers(apps, schema_editor):
+    """save speakers to generate derivative shapes after updating the `shape` attribute
+    """
+    Speaker = apps.get_model("rw", "Speaker")
+    for speaker in Speaker.objects.iterator():
+        speaker.save()
+
 class Migration(migrations.Migration):
     dependencies = [
         ('rw', '0013_speaker_geometry'),
@@ -12,5 +19,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
                 "UPDATE rw_speaker SET shape = ST_Multi(ST_Buffer(ST_MakePoint(longitude, latitude)::geography, maxdistance)::geometry)::geography;"
-        )
+        ),
+        migrations.RunPython(save_speakers)
     ]
